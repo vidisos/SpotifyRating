80fd553eb2f2d5dd989c451f2336052b2958bae4
vidisos <vid.iskra11@gmail.com>
2025-04-16 08:01:43 +0000

    my own html  before magic gpt

--- src/main.py
+++ src/main.py
@@ -6,4 +6,15 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
 app.run(debug=True)
