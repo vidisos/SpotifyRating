643848c93aab820b8103205f725f5326f6a810f8
vidisos <vid.iskra11@gmail.com>
2025-04-17 09:43:26 +0000

    tried to do some login data shenanigans

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -10,9 +10,30 @@ def hello_world():
 @app.route("/login")
 def login():
     return render_template("login.html")
-@app.route("/loginData")
+@app.route("/loginData", methods=["GET", "POST"])
 def loginData():
-    return
+    """
+    if request.method == "GET":
+        return render_template("signup.html")
+    """
+
+    username = request.args.get("username")
+    password = request.args.get("password")
+
+    print(username)
+    print(password)
+
+    if username is True and password is True:
+        return render_template("index.html")
+    
+    if username is True and password is not True:
+        error = "Incorrect password"
+
+    if username is not True:
+        error = "Incorrect username"
+
+    return jsonify({"error": error})
+    
 
 @app.route("/signup")
 def signup():
