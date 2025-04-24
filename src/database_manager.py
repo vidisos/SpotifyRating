82f0f6c9ff49d4d92b842245cbfbb9c031582a61
vidisos <vid.iskra11@gmail.com>
2025-04-24 09:52:16 +0000

    nothing much

--- src/database_manager.py
+++ src/database_manager.py
@@ -5,6 +5,9 @@ DATABASE = "database.db"
 con = sqlite3.connect(DATABASE)
 
 #a cursor allows us to mess with the db
-cur = con.cursor() 
+cur = con.cursor(create_tables) 
 
+cur.executescript(create_tables)
 
+def func1():
+    return
---
+++ src/sql/create_tables.sql
@@ -0,0 +1,6 @@
+create table user(
+    ID int autoincrement,
+    username text,
+    password text, 
+    primary key(ID)
+)
